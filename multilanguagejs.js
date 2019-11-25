0bd6ff1b121929f773099b5686bb7c051941255f
Ian Welerson <ian.nawes@gmail.com>
2019-11-24 22:38:02 -0300

    refactor: Change Ident Spaces

--- multilanguagejs.js
+++ multilanguagejs.js
@@ -11,126 +11,126 @@
 
 class MultilanguageJS {
 
-    /* 
-     * Constructor for MultilanguageJS
-     * - acceptedLanguages: required array of accepted languages
-     * - defaultLanguage: the fallback language
-    */
-    constructor(acceptedLanguages = [], defaultLanguage = '') {
+  /* 
+    * Constructor for MultilanguageJS
+    * - acceptedLanguages: required array of accepted languages
+    * - defaultLanguage: the fallback language
+  */
+  constructor(acceptedLanguages = [], defaultLanguage = '') {
 
-        this._language = '';
-        this._defaultLanguage = defaultLanguage;
-        this._acceptedLanguages = acceptedLanguages;
+    this._language = '';
+    this._defaultLanguage = defaultLanguage;
+    this._acceptedLanguages = acceptedLanguages;
 
-    }
+  }
 
-    /*
-     * Set the the content language
-     * - newLanguage: a string with the language you are setting
-    */
-    setLanguage(newLanguage) {
-        
-        // Checking if the language is in acceptedLanguages
-        if(!this._acceptedLanguages.includes(newLanguage)) {
+  /*
+    * Set the the content language
+    * - newLanguage: a string with the language you are setting
+  */
+  setLanguage(newLanguage) {
+      
+    // Checking if the language is in acceptedLanguages
+    if(!this._acceptedLanguages.includes(newLanguage)) {
 
-            console.log('[Multilanguage] An unaccepted language is trying to be used. The default language or first accepted language has been applied.');
-            
-            this.setLanguage(this.getDefaultLanguage());
+      console.log('[Multilanguage] An unaccepted language is trying to be used. The default language or first accepted language has been applied.');
+      
+      this.setLanguage(this.getDefaultLanguage());
 
-            return;
-
-        }
-
-        this.cleanActualContent();
-
-        this._language = newLanguage;
-        
-        this.showNewContent();
+      return;
 
     }
 
-    /*
-     * Set the page language by browser language
-    */
-    setLanguageByBrowser() {
-        
-        let browserLanguage = navigator.language || navigator.userLanguage; 
+    this.cleanActualContent();
 
-        this.setLanguage(browserLanguage);
-
-    }
-
-    /*
-     * Get the active language
-    */
-    getActiveLanguage() {
-
-        return this._language;
-
-    }
+    this._language = newLanguage;
     
-    /*
-     * Get the array of accepted languages
-    */
-    getAcceptedLanguages() {
+    this.showNewContent();
 
-        return this._acceptedLanguages;
+  }
 
-    }
+  /*
+    * Set the page language by browser language
+  */
+  setLanguageByBrowser() {
+      
+    let browserLanguage = navigator.language || navigator.userLanguage; 
 
-    /*
-     * Get the the default language, used as a fallback
-    */
-    getDefaultLanguage() {
+    this.setLanguage(browserLanguage);
 
-        return this._defaultLanguage ? this._defaultLanguage : this._acceptedLanguages[0];
-        
-    }
+  }
 
-    /*
-     * Search in page for any language 'template' and return all found elements
-    */
-    getLanguageTemplates() {
+  /*
+    * Get the active language
+  */
+  getActiveLanguage() {
+
+    return this._language;
+
+  }
+  
+  /*
+    * Get the array of accepted languages
+  */
+  getAcceptedLanguages() {
+
+    return this._acceptedLanguages;
+
+  }
+
+  /*
+    * Get the the default language, used as a fallback
+  */
+  getDefaultLanguage() {
+
+    return this._defaultLanguage ? this._defaultLanguage : this._acceptedLanguages[0];
+      
+  }
+
+  /*
+    * Search in page for any language 'template' and return all found elements
+  */
+  getLanguageTemplates() {
+  
+    let templates = document.querySelectorAll(`template[type=language-group]`);
     
-        let templates = document.querySelectorAll(`template[type=language-group]`);
-        
-        return templates;
+    return templates;
+  
+  }
+  
+  /*
+    * Use 'template' elements found to show page content according to language
+  */
+  showNewContent() {
+
+    let templates = this.getLanguageTemplates();
+    let actualLanguage = this._language;
     
-    }
+    // Show a log with the language and templates found
+    console.log(`[Multilanguage] ${templates.length} contents translated to ${this._language}.`);
+
+    templates.forEach(function(template) {
+
+      let languageContent = template.content.cloneNode(true).querySelector(`[language=${actualLanguage}]`);
+      
+      template.parentNode.insertBefore(languageContent, template);
+
+    });      
+      
+  }
+  
+  /*
+    * Remove any element with attr 'language'
+  */
+  cleanActualContent() {
+
+    let elements = document.querySelectorAll(`[language]`);
     
-    /*
-     * Use 'template' elements found to show page content according to language
-    */
-    showNewContent() {
+    elements.forEach(function(element) {
+      element.remove();
+    });
 
-        let templates = this.getLanguageTemplates();
-        let actualLanguage = this._language;
-        
-        // Show a log with the language and templates found
-        console.log(`[Multilanguage] ${templates.length} contents translated to ${this._language}.`);
-
-        templates.forEach(function(template) {
-
-            let languageContent = template.content.cloneNode(true).querySelector(`[language=${actualLanguage}]`);
-            
-            template.parentNode.insertBefore(languageContent, template);
-
-        });      
-        
-    }
-    
-    /*
-     * Remove any element with attr 'language'
-    */
-    cleanActualContent() {
-
-        let elements = document.querySelectorAll(`[language]`);
-        
-        elements.forEach(function(element) {
-            element.remove();
-        });
-
-    }
+  }
  
 }
 
