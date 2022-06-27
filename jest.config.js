810ec82d518fb41912a8f2939a79f8c44d706b48
Ian Welerson <ian.nawes@gmail.com>
2022-06-27 20:33:42 -0300

    test: remove __pages__ folder from coverage report

--- jest.config.js
+++ jest.config.js
@@ -26,9 +26,10 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__pages__/'
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
@@ -159,8 +160,8 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  //   '/node_modules/'
+  // ]
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
